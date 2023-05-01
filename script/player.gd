38002b9c777562218f2af87d99875470c2ea43fc
IcyLava <icylavah@gmail.com>
2023-05-01 21:23:34 +0300

    Complete controller support

--- script/player.gd
+++ script/player.gd
@@ -4,6 +4,7 @@ class_name Player
 var velocity: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.RIGHT
 onready var gun := $Rotating/Gun
+var using_controller := false
 
 func _physics_process(delta: float) -> void:
 	# process movement
@@ -11,8 +12,16 @@ func _physics_process(delta: float) -> void:
 	velocity = Game.damp(velocity, input * 1300, 0.001, delta)
 	velocity = move_and_slide(velocity)
 	
+	var aim_direction: Vector2 = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down", 0.4)
+	if aim_direction.length() > 0.5:
+		using_controller = true
+	
 	# face the mouse
-	direction = (get_global_mouse_position() - position).normalized()
+	if using_controller:
+		if aim_direction != Vector2.ZERO:
+			direction = aim_direction.normalized()
+	else:
+		direction = (get_global_mouse_position() - position).normalized()
 	$Rotating.rotation = direction.angle()
 	
 	health = min(health + delta * 0.2, max_health)
@@ -25,6 +34,10 @@ func _physics_process(delta: float) -> void:
 			sound.pitch_scale = pow(2, rand_range(-0.07, 0.07))
 			sound.play()
 
+func _input(event: InputEvent) -> void:
+	var motion := event as InputEventMouseMotion
+	if motion and motion.relative != Vector2.ZERO:
+		using_controller = false
 
 func _on_Player_on_hit() -> void:
 	if health <= 0:
